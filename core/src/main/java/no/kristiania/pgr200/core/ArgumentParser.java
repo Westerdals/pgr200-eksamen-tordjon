419df9371b25b38dedd2d30353510e044e071d19
Unknown <tordjon@gmail.com>
2018-11-04 23:55:58 +0100

    ArgumentParser.getARguments returns hashmap of identifier + value

--- core/src/main/java/no/kristiania/pgr200/core/ArgumentParser.java
+++ core/src/main/java/no/kristiania/pgr200/core/ArgumentParser.java
@@ -1,5 +1,7 @@
 package no.kristiania.pgr200.core;
 
+import java.util.HashMap;
+
 public class ArgumentParser {
 
     /**
@@ -33,6 +35,31 @@ public class ArgumentParser {
             }
         }
         return defaultValue;
+    }
+
+
+
+    public static HashMap<String, String> getArguments(String[] strings) throws IllegalArgumentException {
+        HashMap<String, String> arguments = new HashMap<>();
+        for (int i = 0; i < strings.length; i++) {
+            if(strings[i].startsWith("-")){ //.equalsIgnoreCase(identifier)) {
+
+                if(i >= strings.length - 1 || strings[i + 1].startsWith("-"))
+                    throw new IllegalArgumentException("option (\"-option\") cannot be followed by another option");
+
+                String identifier = strings[i].substring(1);
+                String value = "";
+                while(i < strings.length - 1 && !strings[i + 1].startsWith("-")) {
+                    value += strings[i + 1] + " ";
+                    i++;
+                }
+                // removing trailing " "
+                value = value.substring(0, value.length() - 1);
+
+                arguments.put(identifier, value);
+            }
+        }
+        return arguments;
     }
 
 }
